6530fdb1255800c2de2fdb9cd3b126b5542c28cb
0xM41N <43382783+0xM14N@users.noreply.github.com>
2024-08-31 16:51:11 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config = {
-	color_scheme='Dracula (Official)',
+	color_scheme='Nord (Gogh)',
 	enable_tab_bar=false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
@@ -31,11 +31,20 @@ config = {
     {key="V", mods="CTRL|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
     -- Close current pane with Ctrl+Shift+W
     {key="W", mods="CTRL|SHIFT", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+      {key="1", mods="CTRL", action=wezterm.action{ActivateTab=0}},
+    {key="2", mods="CTRL", action=wezterm.action{ActivateTab=1}},
+    {key="3", mods="CTRL", action=wezterm.action{ActivateTab=2}},
+    {key="4", mods="CTRL", action=wezterm.action{ActivateTab=3}},
+    {key="5", mods="CTRL", action=wezterm.action{ActivateTab=4}},
+    {key="6", mods="CTRL", action=wezterm.action{ActivateTab=5}},
+    {key="7", mods="CTRL", action=wezterm.action{ActivateTab=6}},
+    {key="8", mods="CTRL", action=wezterm.action{ActivateTab=7}},
+    {key="9", mods="CTRL", action=wezterm.action{ActivateTab=8}},
   },
   -- Set the window background opacity
   window_background_opacity = 1,
 
-window_background_image = "C:\\Users\\User\\Downloads\\wapp.jpg",
+window_background_image = "C:\\Users\\User\\Downloads\\lol3.jpg",
 
   window_background_image_hsb = {
     brightness = 0.35,
